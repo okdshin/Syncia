13c2c3612bccb92f32e0904ee92dcabef9bdb21c
s.okada <kokuzen@gmail.com>
2013-01-27 21:57:11 +0900

    fixed.

--- CuiSyncia.cpp
+++ CuiSyncia.cpp
@@ -7,8 +7,15 @@ using namespace neuria::network;
 
 int main(int argc, char* argv[])
 {
+	HostName host_name("localhost");	
+	PortNumber port_number(54321);
+	
+	if(argc > 2){
+		host_name = HostName(std::string(argv[1]));
+		port_number = (boost::lexical_cast<int>(argv[2]));
+	}
 	CuiSyncia cui_syncia(
-		HostName("localhost"), PortNumber(54321),
+		host_name, port_number,
 		neuria::network::BufferSize(256));
 	cui_syncia.InitShell();
 	cui_syncia.InitDispatcher();
